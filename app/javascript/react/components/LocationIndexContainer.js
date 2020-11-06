01d6023fb3f6e0a452ca869be38e050065c9ba8d
Chris Rackey <chrisrackey@MacBook-Pro.local>
2020-11-06 10:23:13 -0500

    my map now renders all my locations as markers

--- app/javascript/react/components/LocationIndexContainer.js
+++ app/javascript/react/components/LocationIndexContainer.js
@@ -1,15 +1,33 @@
 import React, { useEffect, useState } from "react"
-import MapContainer from "./MapContainer"
+import Map from "./Map"
 import LocationIndex from "./LocationIndex"
 import { Link } from 'react-router-dom'
 const LocationIndexContainer = (props) => {
+  const [locations, setLocations] = useState([])
+  useEffect(() => {
+    fetch("/api/v1/locations.json")
+    .then (response => {
+      if (response.ok) {
+        return response
+      } else {
+        let errorMessage = `${response.status} (${response.statusText})`,
+          error = new Error(errorMessage)
+        throw error
+      }
+    })
+    .then(response => response.json())
+    .then(body => {
+      setLocations(body) 
+    })
+    .catch(error => console.error(`Error in fetch: ${error.message}`))
+  }, [])
   return(
     <div>
       <h1>Welcome to Skate Sharez!</h1>
       <h3>These are the spots other users have located near you:</h3>
       <LocationIndex /> 
       <Link className="big" to={`/locations/new`}>Add a New Spot </Link>
-      <MapContainer />
+      <Map locations={locations} />
     </div> 
   )
 }
