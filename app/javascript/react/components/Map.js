2bd32cfd8dd52b5b26b6e8969b5b88acf17be56b
Chris Rackey <chrisrackey@MacBook-Pro.local>
2020-11-06 11:21:51 -0500

    added show links to infowindow box for each marker

--- app/javascript/react/components/Map.js
+++ app/javascript/react/components/Map.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom'
 import { useGoogleMaps } from "react-hook-google-maps"
  
 const Map = (props) => {
@@ -8,15 +8,17 @@ const Map = (props) => {
     "AIzaSyC_YGBXqAn8ejgn1DXEe2nSQh4M9lUULyo",
     {
       center: { lat: 42.3554, lng: -71.0640 },
-      zoom: 14,
+      zoom: 14
     },
   );
   if (map) {
     props.locations.forEach(location => {
       let marker = new google.maps.Marker({ position: {lat: parseFloat(location.lat), lng: parseFloat(location.lng)}, map, name: location.title })
       let infowindow = new google.maps.InfoWindow() 
+      let siteLink = `/locations/${location.id}`
+      let contentString = '<div">' + '<b>' + location.title + '</br>' + '<a href="' + siteLink + '">See more!</a>'
       google.maps.event.addListener(marker, "click", function(evt) {  
-        infowindow.setContent(location.title);
+        infowindow.setContent(contentString);
         infowindow.open(map,this);
       });
     })
